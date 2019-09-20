4e57b23d1552fc1f3f27b083e4edd8d50da6c618
Dave Elfving <elfving@Daves-MacBook.local>
2019-09-20 15:09:38 -0700

    Update setHeight.js

--- curio/setHeight.js
+++ curio/setHeight.js
@@ -3,6 +3,3 @@ let divWidth = document.getElementById('titlebox').offsetWidth;
 let divHeight = divWidth * 0.666 + 'px';
 document.getElementById('titlebox').style.height = divHeight;
 }
-
-window.onload = "setHeight()";
-window.onresize = "setHeight()";
